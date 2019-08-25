84494da390b5278ebc8de6b100dd81639d4a6e6f
gabrielmoro <gabrielhmoro@gmail.com>
2019-08-25 18:24:28 -0300

    Testing some functions

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ a = 0.05
 vid = cv.VideoCapture('vid/WalkByShop1front.mpg')
 
 _, aux1 = vid.read()
-aux2 = cv.cvtColor(aux1, cv.COLOR_BGR2GRAY)
-avg = np.float32(aux2)
+BG = cv.cvtColor(aux1, cv.COLOR_BGR2GRAY)
+avg = np.float32(BG)
 
 krnl = np.ones((5,5),np.uint8)
 
 while(vid.isOpened()):
     ret, frame = vid.read()
-    if (ret == False) or (cv.waitKey(17) == 27):
+    if (ret == False) or (cv.waitKey(10) == 27):
         break
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -23,14 +23,18 @@ while(vid.isOpened()):
 
     diff = cv.absdiff(gray, out)
     _,diff = cv.threshold(diff,20,255,cv.THRESH_BINARY)
-    dilate = cv.dilate(diff, krnl, iterations = 1)
+    
+    noBG = cv.erode(diff, krnl, iterations = 1)
+    noBG = cv.dilate(noBG, krnl, iterations = 1)
+    
+    #dilate = cv.morphologyEx(cv.morphologyEx(diff, cv.MORPH_OPEN, krnl), cv.MORPH_CLOSE, krnl)
 
-    contours, h = cv.findContours(dilate, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours, h = cv.findContours(noBG, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     cv.drawContours(gray, contours, -1, (0, 255, 0), 3)
     
     cv.imshow('Grayscale',gray)
     cv.imshow('Average',out)
-    cv.imshow('No BG', dilate)
+    cv.imshow('No BG', noBG)
 
 
 vid.release()
