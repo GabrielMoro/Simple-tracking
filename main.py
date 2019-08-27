cdfb0e4caf760c6047073eadd0dfcda925ed36ab
gabrielmoro <gabrielhmoro@gmail.com>
2019-08-27 13:41:11 -0300

    Some rectangles

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ _, aux1 = vid.read()
 BG = cv.cvtColor(aux1, cv.COLOR_BGR2GRAY)
 avg = np.float32(BG)
 
-krnl = np.ones((5,5),np.uint8)
+krnl = np.ones((3,3),np.uint8)
 
 while(vid.isOpened()):
     ret, frame = vid.read()
-    if (ret == False) or (cv.waitKey(17) == 27):
+    if (ret == False) or (cv.waitKey(10) == 27):
         break
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -22,14 +22,19 @@ while(vid.isOpened()):
     out = cv.convertScaleAbs(avg)
 
     diff = cv.absdiff(gray, out)
-    _,diff = cv.threshold(diff,20,255,cv.THRESH_BINARY)
+    _, diff = cv.threshold(diff,22,255,cv.THRESH_BINARY)
     
-    noBG = cv.dilate(diff, krnl, iterations = 2)
-    noBG = cv.erode(noBG, krnl, iterations = 2)
+    noBG = cv.erode(diff, krnl, iterations = 1)
+    noBG = cv.dilate(noBG, krnl, iterations = 2)
 
     contours, hs = cv.findContours(noBG, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    cv.drawContours(gray, contours, -1, (0, 255, 0), 3)
-    
+    # cv.drawContours(gray, contours, -1, (0, 255, 0), 3)
+
+    for c in contours:
+        rect = cv.boundingRect(c)
+        x, y, w, h = rect
+        cv.rectangle(gray, (x, y), (x+w, y+h), (0, 255, 0), 2)   
+
     cv.imshow('Grayscale',gray)
     cv.imshow('Average',out)
     cv.imshow('No BG', noBG)
