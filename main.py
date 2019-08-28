ae6537db04d51de3b557db3df07861156130167b
gabrielmoro <gabrielhmoro@gmail.com>
2019-08-28 19:20:08 -0300

    100%

--- main.py
+++ main.py
@@ -18,36 +18,30 @@ krnl = np.ones((3,3),np.uint8)
 
 while(vid.isOpened()):
     ret, frame = vid.read()
-    if (ret == True):
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-        # Background removal
-        cv.accumulateWeighted(gray, avg, a)
-        bg = cv.convertScaleAbs(avg)
-        diff = cv.absdiff(gray, bg)
-        _, diff = cv.threshold(diff,22,255,cv.THRESH_BINARY)
-        
-        # Morphology
-        noBG = cv.erode(diff, krnl, iterations = 2)
-        noBG = cv.dilate(noBG, krnl, iterations = 6)
-        noBG = cv.erode(noBG, krnl, iterations = 5)
-        noBG = cv.dilate(noBG, krnl, iterations = 14)
-
-        # Drawing a rectangle
-        x, y, w, h = cv.boundingRect(noBG)
-        if(w > 45 and w < 120 and h > 45):
-            cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)        
-
-        # Showing and saving the video
-        cv.imshow('Video',frame)
-        out.write(frame)
-        cv.imshow('Average',bg)
-        cv.imshow('No BG', noBG)
-        if (cv.waitKey(7) == 27):
-            break
-    else:
+    if (ret == False or cv.waitKey(7) == 27):
         break
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
+    # Background removal
+    cv.accumulateWeighted(gray, avg, a)
+    bg = cv.convertScaleAbs(avg)
+    diff = cv.absdiff(gray, bg)
+    _, diff = cv.threshold(diff,22,255,cv.THRESH_BINARY)
+    
+    # Morphology
+    noBG = cv.erode(diff, krnl, iterations = 2)
+    noBG = cv.dilate(noBG, krnl, iterations = 6)
+    noBG = cv.erode(noBG, krnl, iterations = 5)
+    noBG = cv.dilate(noBG, krnl, iterations = 14)
+
+    # Drawing a rectangle
+    x, y, w, h = cv.boundingRect(noBG)
+    if(w > 45 and w < 120 and h > 45):
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)        
+
+    # Showing and saving the video
+    cv.imshow('Video',frame)
+    out.write(frame)
     
 out.release()
 vid.release()
